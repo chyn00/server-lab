027fcec89ebfbe0c4d8da981230a776881dc43ba
chyn00 <ybj0749@gmail.com>
2025-06-18 22:17:24 +0900

    init. server에 적용될 기술을 미리 적용

---
+++ src/test/java/com/server/lab/ServerLabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.server.lab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerLabApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
