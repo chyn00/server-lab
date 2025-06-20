a885b2ccfc5a3a9d1bbde5dd34f2bf3d0a2ccc97
chyn00 <ybj0749@gmail.com>
2025-06-20 16:20:22 +0900

    add. aws sdk ipmlementation 추가

--- src/main/java/com/server/lab/file/infra/storage/FileStorageService.java
+++ src/main/java/com/server/lab/file/infra/storage/FileStorageService.java
@@ -1,5 +1,9 @@
 package com.server.lab.file.infra.storage;
 
+import org.springframework.stereotype.Service;
+
+// client와의 협업이 필요한 부분(presignedUrl, sdk 둘 다 필요.)
+@Service
 public class FileStorageService {
 
 }
