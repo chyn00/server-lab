09a55f995baa959e89039040f693755bf1933f26
chyn00 <ybj0749@gmail.com>
2025-06-26 17:36:03 +0900

    update. 중복업로드 되지 않도록 유효성 체크 추가

--- src/main/java/com/server/lab/file/FileController.java
+++ src/main/java/com/server/lab/file/FileController.java
@@ -4,11 +4,11 @@ import com.server.lab.file.application.FileService;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequiredArgsConstructor
@@ -21,16 +21,17 @@ public class FileController {
     return "server-lab";
   }
 
+  //prg 패턴을 활용(post만 사용하면 재요청이 보내질 수 있다. 리다이렉트를 통해, 중복 업로드 방지)
   @GetMapping("/upload")
   public String showUploadForm() {
     return "upload-form"; // 타임리프 리졸버 통해서 html실행(위치 : templates/upload-form.html)
   }
 
   @PostMapping("/upload")
-  public String handleFileUpload(@RequestParam("files") List<MultipartFile> files, Model model) {
+  public String handleFileUpload(@RequestParam("files") List<MultipartFile> files, RedirectAttributes redirectAttributes) {
     List<String> resultMessages = fileService.saveFiles(files);
-    model.addAttribute("messages", resultMessages);
-    return "upload-form";
+    redirectAttributes.addFlashAttribute("messages", resultMessages); // Flash: 리다이렉트 후 1회만 유지됨
+    return "redirect:/upload";
   }
 }
 
--- src/main/java/com/server/lab/file/application/FileService.java
+++ src/main/java/com/server/lab/file/application/FileService.java
@@ -1,28 +1,42 @@
 package com.server.lab.file.application;
 
+import com.server.lab.file.infra.storage.FileStorageService;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
 import org.springframework.web.multipart.MultipartFile;
 
 @Service
+@RequiredArgsConstructor
 public class FileService {
+
+  private final FileStorageService fileStorageService;
 
   public List<String> saveFiles(List<MultipartFile> files) {
     List<String> messages = new ArrayList<>();
 
-    if (files == null || files.isEmpty()) {
+    if (this.hasInvalidUploadFiles(files)) {
       messages.add("업로드할 파일을 선택해주세요.");
     } else {
-      for (MultipartFile file : files) {
-        if (!file.isEmpty()) {
-          messages.add("업로드 성공: " + file.getOriginalFilename());
-        } else {
-          messages.add("빈 파일입니다.");
-        }
-      }
+      return fileStorageService.s3uploadAndMetaDataSave(files);
     }
 
     return messages;
+  }
+
+  public boolean hasInvalidUploadFiles(List<MultipartFile> files) {
+    if (files == null || files.isEmpty()) {
+      return true;
+    }
+
+    //file 내부 이름도 비어있는 파일이 하나라도 있으면 false
+    return files.stream().anyMatch(file ->
+        file == null ||
+            file.isEmpty() ||
+            !StringUtils.hasText(file.getOriginalFilename())
+    );
   }
 }
